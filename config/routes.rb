bac42940ad4f9efa1ce881ffbc4c22488879a25c
michaelpamintuan <michael@paralejasconsulting.com>
2022-12-28 16:52:22 +0800

    Log_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
+
+  get "/dashboard", to: "pages#dashboard"
+
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
