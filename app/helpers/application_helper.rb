3521377918f6a8c97a1414408f20a1ed2d3dbf36
michaelpamintuan <michael@paralejasconsulting.com>
2023-01-05 16:54:41 +0800

    Designing posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+    def main_or_devise_container_class
+        if is_devise_class?
+            "container mx-auto mt-28 px-5 flex"
+        else 
+            "mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"
+      end
+    end
+
+    def is_devise_class?
+        ['sessions', 'registrations', 'passwords'].include?(controller_name)
+    end
+
+    def page_title
+        if controller_name == 'pages'
+            'Dashboard'
+        else 
+            controller_name.titleize
+        end
+    end
+
+    def navigation_active_state cname
+        if cname == controller_name
+            'bg-gray-900 text-white'
+        else
+            'text-gray-300 hover:bg-gray-700 hover:text-white'
+        end
+    end
 end
