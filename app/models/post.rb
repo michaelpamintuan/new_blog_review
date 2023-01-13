6f63f28a42b709825d42957bed39df2db577256c
michaelpamintuan <michael@paralejasconsulting.com>
2023-01-13 17:00:40 +0800

    Public post show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,7 @@ class Post < ApplicationRecord
   mount_uploader :image_url, ImageUploader
   # serialize :image_url, JSON 
   belongs_to :user
+
+  scope :next, lambda {|id| where("id > ?",id).order("id ASC") } # this is the default ordering for AR
+  scope :previous, lambda {|id| where("id < ?",id).order("id DESC") }
 end
